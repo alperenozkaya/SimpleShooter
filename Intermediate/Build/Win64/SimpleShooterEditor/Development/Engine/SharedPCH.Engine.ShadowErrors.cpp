fe2c0a66bde2cd041f67a1d6b5cca4a9a42df9ed
Alperen <ozkayaal@mef.edu.tr>
2023-02-21 03:28:24 +0300

    Initial commit
    Only code, JSON, and txt files included

---
+++ Intermediate/Build/Win64/SimpleShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "E:/UnrealCourse/SimpleShooter/Intermediate/Build/Win64/SimpleShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+
+class AGun;
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	UFUNCTION(BlueprintPure)
+	bool IsDead() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercent() const;
+	
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	void Shoot();
+private:
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+	
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 10.f;
+	UPROPERTY(EditDefaultsOnly)
+	float MaxHealth = 100.f;
+
+	UPROPERTY(VisibleAnywhere)
+	float Health;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AGun> GunClass;
+
+	UPROPERTY()
+	AGun* Gun;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TArray<TSubclassOf<AGun>> GunClasses;
+	TArray<AGun*> Guns;
+	int ActiveGunIndex;
+
+	void SwitchGun(int Delta);
+	void SwitchGunToIndex(int Index);
+
+	// properties for gun array version
+ 
+	
+
+
+	
+
+};
